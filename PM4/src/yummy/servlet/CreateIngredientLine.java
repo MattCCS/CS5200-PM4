67e102bff5a63813360f6eced3f4038866debd37
krisdiao <527369570@qq.com>
2020-07-11 17:57:16 -0400

    Update CreateIngredientLine.java

--- PM4/src/yummy/servlet/CreateIngredientLine.java
+++ PM4/src/yummy/servlet/CreateIngredientLine.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-@WebServlet("/createIngredient")
+@WebServlet("/createIngredientLine")
 public class CreateIngredientLine extends HttpServlet {
 
     protected IngredientLineDao ingredientLineDao;
