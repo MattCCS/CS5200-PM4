b1b0c2e746c2254e5a721ab7ed7a6b192b0786da
krisdiao <527369570@qq.com>
2020-07-11 19:08:02 -0400

    Add files via upload

---
+++ PM4/src/yummy/servlet/ReadFood.java
@@ -0,0 +1,79 @@
+package yummy.servlet;
+
+import yummy.dal.*;
+import yummy.model.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.annotation.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/readFood")
+public class ReadFood extends HttpServlet {
+
+	protected FoodDao foodDao;
+
+	@Override
+	public void init() throws ServletException {
+		foodDao = FoodDao.getInstance();
+	}
+
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+        Map<String, String> messages = new HashMap<String, String>();
+        req.setAttribute("messages", messages);
+
+        List<Food> results = new ArrayList<Food>();
+
+        String id = req.getParameter("id");
+        if (id == null || id.trim().isEmpty()) {
+            messages.put("success", "Please enter a valid id.");
+        } else {
+        	try {
+        		results.add(foodDao.getById(Integer.parseInt(id)));
+            } catch (SQLException e) {
+    			e.printStackTrace();
+    			throw new IOException(e);
+            }
+        	messages.put("success", "Displaying results for " + id);
+        }
+        req.setAttribute("results", results);
+
+        req.getRequestDispatcher("/ReadFood.jsp").forward(req, resp);
+	}
+	
+	@Override
+    public void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Map<String, String> messages = new HashMap<String, String>();
+        req.setAttribute("messages", messages);
+
+        List<Food> results = new ArrayList<Food>();
+
+        String id = req.getParameter("id");
+        System.out.println(id);
+        if (id == null || id.trim().isEmpty()) {
+            messages.put("success", "[POST] Please enter a valid id.");
+        } else {
+            try {
+                results.add(foodDao.getById(Integer.parseInt(id)));
+            } catch (SQLException e) {
+                e.printStackTrace();
+                throw new IOException(e);
+            }
+            messages.put("success", "Displaying results for " + id);
+        }
+        req.setAttribute("results", results);
+
+        req.getRequestDispatcher("/ReadFood.jsp").forward(req, resp);
+    }
+}
